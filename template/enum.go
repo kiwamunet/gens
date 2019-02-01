d51d991d832d428a0ec3956fcddb480d57a4df96
kiwamunet <kiwamunet@gmail.com>
2019-02-01 18:42:35 +0900

    bug fix

--- template/enum.go
+++ template/enum.go
@@ -1,21 +1,28 @@
 package template
 
 var EnumTmpl = `{{$type := .Type}}{{$key := .Key}}
-type {{.Type}}_{{$key}} int
+type {{.Type}}_{{$key}} int64
 
 const (
 	{{range $index, $var := .Member}}{{ if eq $index 0 }}{{$var}}_{{$key}} {{$type}}_{{$key}} = iota
 	{{else}}{{$var}}_{{$key}}
 	{{end}}{{end}}
 )
-	
-func (em {{.Type}}_{{$key}}) String() string {
-	switch em {
-	{{range $index, $var := .Member}}case {{$var}}_{{$key}}:
-		return "{{$var}}"
-	{{end}}default:
-		return "UNKNOWN"
-	}	
+
+var {{.Type}}_{{$key}} = [...]string{
+	{{range $index, $var := .Member}}"{{$var}}",
+	{{end}}
 }
+
+func (em {{.Type}}_{{$key}}) String() string {
+	return {{.Type}}_{{$key}}[em]
+}
+
+func (e *{{.Type}}_{{$key}}) Scan(value interface{}) error {
+	*e = {{.Type}}_{{$key}}(value.(int64))
+	return nil
+}
+
+func (e {{.Type}}_{{$key}}) Value() (driver.Value, error) { return int64(e), nil }
 
 `
