a043db1dcb8df774b4336e83c85cedf791cc810a
kiwamunet <kiwamunet@gmail.com>
2019-02-01 17:32:49 +0900

    miss

--- template/enum.go
+++ template/enum.go
@@ -3,14 +3,14 @@ package template
 var EnumTmpl = `type {{.Type}} int
 {{$type := .Type}}
 const (
-	{{range $index, $var := .Member}}{{ if eq $index 0 }}{{$var}} {{$type}} = iota
-	{{else}}{{$var}}
+	{{range $index, $var := .Member}}{{ if eq $index 0 }}{{$var}}_{{$type}} {{$type}} = iota
+	{{else}}{{$var}}_{{$type}}
 	{{end}}{{end}}
 )
 	
 func (em {{.Type}}) String() string {
 	switch em {
-	{{range $index, $var := .Member}}case {{$var}}:
+	{{range $index, $var := .Member}}case {{$var}}_{{$type}}:
 		return "{{$var}}"
 	{{end}}default:
 		return "UNKNOWN"
