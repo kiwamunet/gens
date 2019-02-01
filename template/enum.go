b73f835d3c832d7029dea6eeefdc11c086503b18
kiwamunet <kiwamunet@gmail.com>
2019-02-01 17:43:55 +0900

    mis

--- template/enum.go
+++ template/enum.go
@@ -1,14 +1,15 @@
 package template
 
-var EnumTmpl = `type {{.Type}} int
-{{$type := .Type}}{{$key := .Key}}
+var EnumTmpl = `{{$type := .Type}}{{$key := .Key}}
+type {{.Type}}_{{$key}} int
+
 const (
-	{{range $index, $var := .Member}}{{ if eq $index 0 }}{{$var}}_{{$key}} {{$type}} = iota
+	{{range $index, $var := .Member}}{{ if eq $index 0 }}{{$var}}_{{$key}} {{$type}}_{{$key}} = iota
 	{{else}}{{$var}}_{{$key}}
 	{{end}}{{end}}
 )
 	
-func (em {{.Type}}) String() string {
+func (em {{.Type}}_{{$key}}) String() string {
 	switch em {
 	{{range $index, $var := .Member}}case {{$var}}_{{$key}}:
 		return "{{$var}}"
